fb595065ca9e677db534090ea9d80895bf3c969f
Mikhail Bahatyrou <mikhailbahatyrou.job@gmail.com>
2017-05-10 14:36:57 +0300

    new repository

---
+++ src/main/java/com/epam/trjava/jenkinstest/RunApp.java
@@ -0,0 +1,12 @@
+package main.java.com.epam.trjava.jenkinstest;
+
+/**
+ * Created by Mikhail on 09.05.2017.
+ */
+public class RunApp {
+
+    public static void main(String argds[]){
+        int build = 0;
+        System.out.println("--------------- build "+ build + " ---------------");
+    }
+}
