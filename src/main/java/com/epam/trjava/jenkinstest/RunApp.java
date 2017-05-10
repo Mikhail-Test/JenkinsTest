c5f46ec0e01f5edceb6110e8fc7e2ae3905aa6d9
Mikhail Bahatyrou <mikhailbahatyrou.job@gmail.com>
2017-05-10 16:54:57 +0300

    build 5

--- src/main/java/com/epam/trjava/jenkinstest/RunApp.java
+++ src/main/java/com/epam/trjava/jenkinstest/RunApp.java
@@ -6,7 +6,7 @@ package main.java.com.epam.trjava.jenkinstest;
 public class RunApp {
 
     public static void main(String argds[]){
-        int build = 4;
+        int build = 5;
         System.out.println("--------------- build "+ build + " ---------------");
     }
 }
